0ece1d9500146c6838d352d5dff4846343f25198
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2023-11-26 14:02:34 +0200

    add key stor

--- lib/main.dart
+++ lib/main.dart
@@ -213,8 +213,7 @@ class ResiThon extends StatelessWidget {
         theme: themeDataLight,
         builder: (context, child) {
           return MediaQuery(
-            data: MediaQuery.of(context)
-                .copyWith(textScaler: const TextScaler.linear(1.0)),
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
             child: child!,
           );
         },
